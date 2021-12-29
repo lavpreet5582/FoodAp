fb4a31ba499ce516c1efec7793bf8814528e9cbe
lavpreet5582 <lsv2885655@gmail.com>
2021-12-29 22:28:15 +0530

    updates

---
+++ model/userModel.js
@@ -0,0 +1,83 @@
+const mongoose = require("mongoose");
+const validator = require("email-validator");
+const bcrypt = require("bcrypt");
+// const ratelimit=require
+
+const {db_link} = require('../secrets');
+mongoose
+    .connect(db_link)
+    .then(function (connection) {
+        console.log("db has been conncetd")
+    }).catch(function (error) {
+        console.log("err", error);
+    })
+//mongoose -> data -> exact -> data -> that is required to form an entity 
+//  data completness , data validation
+// name ,email,password,confirmPassword-> min ,max,confirmPassword,required ,unique 
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            validate: function () {
+                // third party library 
+                return validator.validate(this.email)
+            }
+        },
+        password: {
+            type: String,
+            required: true,
+            minlength: 8,
+        }
+        ,
+        confirmPassword: {
+            type: String,
+            required: true,
+            minlength: 8,
+            validate: function () {
+                return this.password == this.confirmPassword
+            }
+        },
+        createdAt: {
+            type: String,
+
+        },
+        token: String,
+        validUpto: Date,
+        role: {
+            type: String,
+            enum: ["admin", "ce", "user"],
+            default: "user"
+        },
+        bookings: {
+            //   array of object id 
+            type: [mongoose.Schema.ObjectId],
+            ref: "bookingModel"
+        },
+    })
+// hook
+userSchema.pre('save',async  function (next) {
+    // do stuff
+    const salt = await bcrypt.genSalt(10);
+    // password convert text
+    this.password = await bcrypt.hash(this.password, salt);
+    this.confirmPassword = undefined;
+    next();
+});
+// document method
+userSchema.methods.resetHandler = async function (password, confirmPassword) {
+    const salt = await bcrypt.genSalt(10);
+    // password convert text
+    this.password = await bcrypt.hash(this.password, salt);
+
+    this.confirmPassword = confirmPassword;
+    this.token = undefined;
+}
+// model
+let userModel = mongoose.model("PABUserModel", userSchema);
+module.exports = userModel;
---
+++ secrets.js
@@ -0,0 +1,10 @@
+module.exports = {
+     JWT_SECRET : "lovesinghmanka12345",
+     db_link : "mongodb+srv://LavpreetSingh_5582:Mm8bZFpZd0mfYMmw@cluster0.ppxiu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+     ,email:"lsv2885657@gmail.com",
+     mailpass:"zctxyettfecjtzfo",
+     KEY_ID:'rzp_test_WSs0pXktBnzQoj',
+     KEY_SECRET:'dgjDrXlvByyPqfFOWGevq30A'
+
+}
+//Mm8bZFpZd0mfYMmw
